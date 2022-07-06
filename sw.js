492028927ca40a039bd84e7260aa96400311474b
TheTridentGuy <99673331+TheTridentGuy@users.noreply.github.com>
2022-07-06 07:46:50 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -31,8 +31,8 @@ self.addEventListener('install', function (e) {
   e.waitUntil(
     caches.open(CACHE_NAME).then(function (cache) {
       console.log('Installing cache : ' + CACHE_NAME);
-      return cache.addAll(URLS)
-    })
+      return cache.addAll(URLS);
+    });
   )
 })
 
@@ -40,13 +40,13 @@ self.addEventListener('activate', function (e) {
   e.waitUntil(
     caches.keys().then(function (keyList) {
       var cacheWhitelist = keyList.filter(function (key) {
-        return key.indexOf(APP_PREFIX)
+        return key.indexOf(APP_PREFIX);
       })
       cacheWhitelist.push(CACHE_NAME);
       return Promise.all(keyList.map(function (key, i) {
         if (cacheWhitelist.indexOf(key) === -1) {
           console.log('Deleting cache : ' + keyList[i] );
-          return caches.delete(keyList[i])
+          return caches.delete(keyList[i]);
         }
       }))
     })
